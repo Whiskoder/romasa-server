d52ff70aa64c1842b2ae8f0214b451e7090d0263
whiskoder <whiskoder@gmail.com>
2025-09-29 19:02:15 -0600

    service operations entity

--- src/modules/service-operations/entities/service-operations.entity.ts
+++ src/modules/service-operations/entities/service-operations.entity.ts
@@ -1,4 +1,55 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity({})
-export class ServiceOperations {}
+@Entity({ name: 'service_operations' })
+export class ServiceOperations {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'bit', default: false })
+  affectsOperability: boolean;
+
+  @Column({ type: 'datetime', nullable: false })
+  createdAt: number;
+
+  @Column({ type: 'int', nullable: false })
+  createdByEmployeeId: number;
+
+  @Column({ type: 'datetime', nullable: true })
+  estimatedDeliveryDate: Date;
+
+  @Column({ type: 'int', nullable: false })
+  departmentManagerEmployeeId: number;
+
+  // - Datos del `mecanico`
+
+  @Column({ type: 'varchar', length: 10, nullable: false })
+  folio: string;
+
+  @Column({ type: 'varchar', length: 10, default: 'low' })
+  priority: string;
+
+  @Column({ type: 'varchar', length: 20, default: 'created' })
+  status: string;
+
+  @Column({ type: 'datetime', nullable: false })
+  updatedAt: number;
+
+  @Column({ type: 'int', nullable: false })
+  vehicleDriverEmployeeId: number;
+
+  @Column({ type: 'nvarchar', length: 1000, nullable: false })
+  vehicleFailure: string;
+
+  @Column({ type: 'int', nullable: false })
+  vehicleFuelLevel: number;
+
+  @Column({ type: 'int', nullable: false })
+  vehicleId: number;
+
+  // TODO
+  @Column({ type: 'nvarchar', length: 'MAX', nullable: true })
+  vehicleInventory: string;
+
+  @Column({ type: 'int', nullable: false })
+  vehicleMileage: number;
+}
